cb0b9061def0a7b2633303314766ec0a9280ee37
elen-oz <ozerova.lenka@gmail.com>
2024-01-05 21:56:55 +0100

    temp change checking for email

--- frontend/src/schemas/index.js
+++ frontend/src/schemas/index.js
@@ -3,10 +3,10 @@ import * as yup from 'yup';
 export const loginSchema = yup.object().shape({
   email: yup
     .string()
-    .email('Please enter a valid email')
+    // .email('Please enter a valid email')
     .required('Required field'),
   password: yup
     .string()
-    .min(6, 'Password should be at least 6 characters')
+    .min(5, 'Password should be at least 6 characters')
     .required('Required field'),
 });
