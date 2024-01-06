a137baaf558d263cec51eb506264836141e78cfe
elen-oz <ozerova.lenka@gmail.com>
2024-01-06 21:11:45 +0100

    fix: server response

--- frontend/src/schemas/index.js
+++ frontend/src/schemas/index.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 
 export const loginSchema = yup.object().shape({
-  email: yup
+  username: yup
     .string()
     // .email('Please enter a valid email')
     .required('Required field'),
