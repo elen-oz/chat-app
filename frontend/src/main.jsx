247d3a44966bf1130669446e1fbf1c7a021be98a
elen-oz <ozerova.lenka@gmail.com>
2024-01-07 13:41:08 +0100

    feat: unable dark mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  ColorModeProvider,
+  ColorModeScript,
+} from '@chakra-ui/react';
 import App from './App.jsx';
+import theme from './theme';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <ColorModeProvider>
-        <App />
-      </ColorModeProvider>
+      {/* <ColorModeProvider> */}
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+      {/* </ColorModeProvider> */}
     </ChakraProvider>
   </React.StrictMode>
 );
