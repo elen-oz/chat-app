bf8708fc804d660f7b880bc3a6ccbdf075ff997b
elen-oz <ozerova.lenka@gmail.com>
2024-01-11 20:56:04 +0100

    feat: 4.1: Receive data from the server when an authorized user opens a chat page

--- frontend/src/pages/PrivatePage.jsx
+++ frontend/src/pages/PrivatePage.jsx
@@ -1,36 +1,68 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import routes from '../routes';
-import { Flex, Text } from '@chakra-ui/react';
-
-const getAuthHeader = () => {
-  const userId = JSON.parse(localStorage.getItem('userId'));
-
-  if (userId && userId.token) {
-    console.log(`Authorized`);
-    return { Authorization: `Bearer ${userId.token}` };
-  }
-
-  return {};
-};
+import { Flex, Text, Box, Heading } from '@chakra-ui/react';
 
 const PrivatePage = () => {
-  const [content, setContent] = useState('');
+  const [channels, setChannels] = useState([]);
+  const [messages, setMessages] = useState([]);
+
+  const axiosInstance = axios.create({
+    baseURL: 'http://localhost:5001',
+  });
+
   useEffect(() => {
     const fetchContent = async () => {
-      const { data } = await axios.get(routes.usersPath(), {
-        headers: getAuthHeader(),
-      });
+      try {
+        const { data } = await axiosInstance.get(routes.usersPath(), {
+          headers: getAuthHeader(),
+        });
+
+        if (data && data.channels && data.messages) {
+          setChannels(data.channels);
+          setMessages(data.messages);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     };
 
     fetchContent();
   }, []);
 
+  const getAuthHeader = () => {
+    const userId = JSON.parse(localStorage.getItem('userId'));
+
+    if (userId && userId.token) {
+      console.log(`Authorized`);
+      return { Authorization: `Bearer ${userId.token}` };
+    }
+
+    return {};
+  };
+
   return (
     <Flex bg='gray.100' align='center' justify='center' h='100%'>
       <Text>Private Page 🤫</Text>
-      {content && <Text>{content}</Text>}
+      <Heading>Chat</Heading>
+      {channels.length > 0 && (
+        <Box>
+          <Text>Channels:</Text>
+          {channels.map((channel) => (
+            <Text key={channel.id}>{channel.name}</Text>
+          ))}
+        </Box>
+      )}
+      {messages.length > 0 && (
+        <Box>
+          <Text>Messages:</Text>
+          {messages.map((message) => (
+            <Text key={message.id}>{message.text}</Text>
+          ))}
+        </Box>
+      )}
     </Flex>
   );
 };
+
 export default PrivatePage;
